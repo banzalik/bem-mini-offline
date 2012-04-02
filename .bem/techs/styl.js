f235cc4f1fe93369ed9c9a70674c517f2ca1307f
Alexander Boythenko <banzalik@gmail.com>
2012-04-03 00:05:27 +0300

    add styl tech

---
+++ .bem/techs/styl.js
@@ -0,0 +1,12 @@
+var myPath = require('bem/lib/path'),
+    Template = require('bem/lib/template'),
+    INHERIT = require('inherit'),
+    CssTech = require('bem/lib/techs/css').Tech;
+
+exports.Tech = INHERIT(CssTech, {
+
+    getBuildResultChunk: function(relPath, path, suffix) {
+        return '@import "' + relPath + '"\n';
+    }
+
+});
